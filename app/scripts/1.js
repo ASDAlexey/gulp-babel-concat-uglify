3c018f40195e9078e5a3c12312fcf6b5e284be47
Home <asdalexey@yandex.ru>
2017-03-18 00:04:24 +0300

    init gulp

---
+++ app/scripts/1.js
@@ -0,0 +1,27 @@
+console.log('1');
+
+
+class Asd {
+  constructor() {
+
+    console.log('aaa');
+
+
+
+
+    console.log('class')
+  }
+}
+
+
+const a = () => {
+
+
+
+
+
+
+  console.log('fn a')
+};
+a();
+new Asd();
---
+++ dist/all.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+console.log('1');
+
+var Asd = function Asd() {
+  _classCallCheck(this, Asd);
+
+  console.log('aaa');
+
+  console.log('class');
+};
+
+var a = function a() {
+
+  console.log('fn a');
+};
+a();
+new Asd();
+'use strict';
+
+console.log('2');
+
+console.log('aa55');
+//# sourceMappingURL=all.js.map
