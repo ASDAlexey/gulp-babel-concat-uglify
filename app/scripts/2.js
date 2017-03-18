a039ad5409daf1c1cb99fdbb69b95fabfa7e85ee
Home <asdalexey@yandex.ru>
2017-03-19 01:17:40 +0300

    created replace-babel-concat-uglify

--- app/scripts/2.js
+++ app/scripts/2.js
@@ -6,3 +6,17 @@ console.log('2');
 
 
 console.log('aa55');
+const d = () => {
+
+
+
+
+
+
+
+
+
+  console.log('fn a')
+};
+
+d();
--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,28 @@ var gulpCopy = require('gulp-copy');
 var rename = require("gulp-rename");
 var runSequence = require('run-sequence');
 var htmlreplace = require('gulp-html-replace');
+var useref = require('useref');
+var gulpUseref = require('gulp-useref');
+var fs = require('fs');
 
-gulp.task('clean', function () {
+gulp.task('clean:dist', function () {
   return gulp.src('dist/*')
              .pipe(clean({ force: true }))
 });
 
-gulp.task('assets', function () {
-  return gulp.src('app/index.html').pipe(gulp.dest('dist'));
+gulp.task('clean:dist-scripts', function () {
+  return gulp.src('dist/scripts/*')
+             .pipe(clean({ force: true }))
+});
+
+// gulp.task('assets', function () {
+//   return gulp.src('app/index.html').pipe(gulp.dest('dist'));
+// });
+
+gulp.task('htmlreplace', function () {
+  return gulp.src('app/*.html')
+             .pipe(gulpUseref())
+             .pipe(gulp.dest('dist'));
 });
 
 gulp.task('babel', function () {
@@ -34,19 +48,55 @@ gulp.task('babel', function () {
              .pipe(gulp.dest('dist'))
 });
 
-gulp.task('htmlreplace', function () {
-  gulp.src('index.html')
-      .pipe(htmlreplace({
-        'css': 'styles.min.css',
-        'js': 'js/bundle.min.js'
-      }))
-      .pipe(gulp.dest('build/'));
+gulp.task('useref', function () {
+  fs.readFile('./app/index.html', function (err, html) {
+    if (err) throw err;
+
+    const result = useref(html.toString())[1];
+    const { css, js } = result;
+    const data = {
+      css: [],
+      js: [],
+    };
+
+    Object.keys(css).forEach(key => {
+      if (key && css[key].assets && css[key].assets.length) {
+        data.css.push({
+          name: key,
+          arr: css[key].assets.map((file) => ('app/' + file)),
+        })
+      }
+    });
+    Object.keys(js).forEach(key => {
+      if (key && js[key].assets && js[key].assets.length) {
+        data.js.push({
+          name: key,
+          arr: js[key].assets.map((file) => ('app/' + file)),
+        })
+      }
+    });
+
+    data.js.forEach(item => {
+      gulp.src(item.arr)
+          .pipe(sourcemaps.init())
+          .pipe(babel({
+            presets: ['es2015']
+          }))
+          .pipe(concat(item.name))
+          .pipe(uglify())
+          .pipe(sourcemaps.write('.'))
+          .pipe(gulp.dest('dist'))
+    });
+  });
 });
 
 gulp.task('default', function (callback) {
-  runSequence('clean',
-    'assets',
-    'babel',
-    // 'htmlreplace',
+  runSequence(
+    'clean:dist',
+    // 'assets',
+    // 'babel',
+    'htmlreplace',
+    'clean:dist-scripts',
+    'useref',
     callback);
 });
