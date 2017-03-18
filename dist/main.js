fa934a2833227c1497a2ed5096fa362b0207398b
Home <asdalexey@yandex.ru>
2017-03-18 18:54:51 +0300

    created babel-concat-uglify

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+"use strict";function _classCallCheck(o,l){if(!(o instanceof l))throw new TypeError("Cannot call a class as a function")}console.log("1");var Asd=function o(){_classCallCheck(this,o),console.log("aaa"),console.log("class")},a=function(){console.log("fn a")};a(),new Asd,console.log("2"),console.log("aa55");
+//# sourceMappingURL=main.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,50 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 var clean = require('gulp-clean');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var cleanCss = require('gulp-clean-css');
+var rev = require('gulp-rev');
+var gulpCopy = require('gulp-copy');
+var rename = require("gulp-rename");
+var runSequence = require('run-sequence');
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task('clean', function () {
   return gulp.src('dist/*')
              .pipe(clean({ force: true }))
 });
 
+gulp.task('assets', function () {
+  return gulp.src('app/index.html').pipe(gulp.dest('dist'));
+});
+
 gulp.task('babel', function () {
-  return gulp.src('app/scripts/**/*.js')
+  return gulp.src('app/scripts/*.js')
              .pipe(sourcemaps.init())
              .pipe(babel({
                presets: ['es2015']
              }))
-             .pipe(concat('all.js'))
+             .pipe(concat('main.js'))
+             .pipe(uglify())
              .pipe(sourcemaps.write('.'))
              .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', [
-  'clean',
-  'babel',
-]);
+gulp.task('htmlreplace', function () {
+  gulp.src('index.html')
+      .pipe(htmlreplace({
+        'css': 'styles.min.css',
+        'js': 'js/bundle.min.js'
+      }))
+      .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', function (callback) {
+  runSequence('clean',
+    'assets',
+    'babel',
+    // 'htmlreplace',
+    callback);
+});
